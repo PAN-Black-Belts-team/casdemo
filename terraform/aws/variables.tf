7ad3f5e97fde1663c552825a478ae0a913ae9d44
Mike Roberts <mroberts@paloaltonetworks.com>
2023-01-23 11:02:50 -0500

    initial commit

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+  default = "ami-063a9ea2ff5685f7f"
+}
