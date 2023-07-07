199abae3ecb8068112f6c67e63e8c72a5a076695
jmagee70 <jmagee@paloaltonetworks.com>
2023-07-07 11:45:07 +0100

    secrets

--- terraform/azure/storage.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_storage_account" "test_blob" {
-    name = var.st.name
-    resource_group_name = var.rg_shared_name
-    location = var.rg_shared_location
-    account_tier = var.st.tier
-    account_replication_type = var.st.replication
-    public_network_access_enabled = true
-}
